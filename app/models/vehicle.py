da35fb4a37505f21995ad1b45e03864d8197470c
AmauryRomeroDev <amaury_romero.dev@hotmail.com>
2026-03-22 22:50:35 -0600

    docs: Create new model
    Add vehicle model

---
+++ app/models/vehicle.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer,String, Boolean, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
+from database.connection import Base
+
+class vehicle(Base):
+    __tablename__="vehicles"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    client_id = Column(Integer, ForeignKey("clients.id", ondelete="CASCADE"))
+    
+    liscence_plate = Column(String(20), nullable=False) 
+    brand = Column(String(50), nullable=False) 
+    model = Column(String(50), nullable=False) 
+    vehicle_type = Column(String(50), nullable=False) 
+    
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    client=relationship("Client",back_populates="vehicles" )
